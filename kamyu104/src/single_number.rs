c2b607238ace50a6c1045a9e61f1f00cb957bb55
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-07-18 00:06:42 +0800

    fix

--- kamyu104/src/single_number.rs
+++ kamyu104/src/single_number.rs
@@ -13,6 +13,6 @@ mod tests {
     #[test]
     fn test_single_number() {
         assert_eq!(single_number(vec![2,2,1]), 1);
-        assert_ne!(single_number(vec![4,1,2,1,2]), 4);
+        assert_eq!(single_number(vec![4,1,2,1,2]), 4);
     }
 }

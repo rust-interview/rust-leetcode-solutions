39857c2aecd3d5b8f94ac828bb506632b2c71d8c
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-07-17 23:48:52 +0800

    add resource

--- kamyu104/src/single_number.rs
+++ kamyu104/src/single_number.rs
@@ -3,7 +3,7 @@
 
 #[allow(dead_code)]
 pub fn single_number(nums: Vec<i32>) -> i32 {
-    1
+    nums.iter().fold(0, |acc, &x| acc ^ x)
 }
 
 #[cfg(test)]
@@ -12,7 +12,7 @@ mod tests {
 
     #[test]
     fn test_single_number() {
-        assert_eq!(single_number(!vec[2,2,1]), 1);
-        assert_ne!(single_number(!vec[4,1,2,1,2]), 4);
+        assert_eq!(single_number(vec![2,2,1]), 1);
+        assert_ne!(single_number(vec![4,1,2,1,2]), 4);
     }
 }

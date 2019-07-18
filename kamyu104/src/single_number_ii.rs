628e87da4ef7f807cd3a54bdb409cbc824a1b1c9
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-07-19 01:02:21 +0800

    add sol4

--- kamyu104/src/single_number_ii.rs
+++ kamyu104/src/single_number_ii.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub struct Solution1 {}
 pub struct Solution2 {}
 pub struct Solution3 {}
@@ -30,31 +32,19 @@ impl Solution2 {
     }
 }
 
-impl Solution3 {
-    pub fn single_number(nums: Vec<i32>) -> i32 {
-        let (mut one, mut two, mut carry): (i32, i32, i32) = (0, 0, 0);
-        for num in nums.iter() {
-            two |= one & num;
-            one ^= num;
-            carry = one & two;
-            one &= !carry;
-            two &= !carry;
-        }
-        one
-    }
-}
+// https://github.com/kamyu104/LeetCode-Solutions/blob/master/Python/single-number-ii.py#L31
+// impl Solution3 {
+//     pub fn single_number(nums: Vec<i32>) -> i32 {
+//     }
+// }
 
 impl Solution4 {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        let (mut one, mut two, mut carry): (i32, i32, i32) = (0, 0, 0);
-        for num in nums.iter() {
-            two |= one & num;
-            one ^= num;
-            carry = one & two;
-            one &= !carry;
-            two &= !carry;
-        }
-        one
+        let sum: i32 = nums.iter().sum();
+        let set_sum: i32 = nums.iter().cloned()
+                            .collect::<HashSet<i32>>()
+                            .iter().sum();
+        (3* set_sum -sum)/2
     }
 }
 
@@ -72,9 +62,9 @@ mod tests {
         assert_eq!(Solution2::single_number(vec![0,1,0,1,0,1,99]), 99);
         assert_eq!(Solution2::single_number(vec![0, 0, 0, 1, 1, 1, 5]), 5);
 
-        assert_eq!(Solution3::single_number(vec![2,2,3,2]), 3);
-        assert_eq!(Solution3::single_number(vec![0,1,0,1,0,1,99]), 99);
-        assert_eq!(Solution3::single_number(vec![0, 0, 0, 1, 1, 1, 5]), 5);
+        // assert_eq!(Solution3::single_number(vec![2,2,3,2]), 3);
+        // assert_eq!(Solution3::single_number(vec![0,1,0,1,0,1,99]), 99);
+        // assert_eq!(Solution3::single_number(vec![0, 0, 0, 1, 1, 1, 5]), 5);
 
         assert_eq!(Solution4::single_number(vec![2,2,3,2]), 3);
         assert_eq!(Solution4::single_number(vec![0,1,0,1,0,1,99]), 99);

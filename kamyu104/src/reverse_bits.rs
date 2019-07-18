a90e129d23e33cd8c660ead3c18225795cc5eb36
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-07-19 01:54:45 +0800

    fix

--- kamyu104/src/reverse_bits.rs
+++ kamyu104/src/reverse_bits.rs
@@ -1,12 +1,18 @@
 // Time : O(logn) = O(32)
 // Space: O(1)
 
-pub struct Solution1 {}
-pub struct Solution2 {}
+pub struct Solution {}
 
-impl Solution1 {
-    pub fn reverse_bits(a: u32) -> u32 {
-        a
+impl Solution {
+    pub fn reverse_bits(mut n: u32) -> u32 {
+        let mut result: u32 = 0;
+        for _ in (0..32) {
+            result <<= 1;
+            result |= n & 1;
+            n >>= 1;
+        }
+        
+        result
     }
 }
 
@@ -16,6 +22,7 @@ mod tests {
 
     #[test]
     fn test_reverse_bits() {
-        assert_eq!(Solution1::reverse_bits(u32::from_str_radix("01110011001", 2).unwrap()), u32::from_str_radix("01110011001", 2).unwrap());
+        assert_eq!(Solution::reverse_bits(u32::from_str_radix("00000010100101000001111010011100", 2).unwrap()), u32::from_str_radix("00111001011110000010100101000000", 2).unwrap());
+        assert_eq!(Solution::reverse_bits(u32::from_str_radix("11111111111111111111111111111101", 2).unwrap()), u32::from_str_radix("10111111111111111111111111111111", 2).unwrap());
     }
 }

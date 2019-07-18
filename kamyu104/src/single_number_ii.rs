eb426cfec6aff293afd254e64102c4aa46f6192f
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-07-18 22:57:57 +0800

    add single_number_ii

--- kamyu104/src/single_number_ii.rs
+++ kamyu104/src/single_number_ii.rs
@@ -1,8 +1,15 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn single_number(nums: Vec<i32>) -> i32 {
-        1
+    pub fn single_number1(nums: Vec<i32>) -> i32 {
+        let (mut one, mut two): (i32, i32) = (0, 0);
+        for num in nums.iter() {
+            let new_one = (!num & one) | (num & !one & !two);
+            let new_two = (!num & two) | (num & one);
+            one = new_one;
+            two = new_two;
+        }
+        one
     }
 }
 
@@ -13,7 +20,8 @@ mod tests {
 
     #[test]
     fn test_add() {
-        assert_eq!(Solution::single_number(vec![2,2,3,2]), 3);
-        assert_eq!(Solution::single_number(vec![0,1,0,1,0,1,99]), 99);
+        assert_eq!(Solution::single_number1(vec![2,2,3,2]), 3);
+        assert_eq!(Solution::single_number1(vec![0,1,0,1,0,1,99]), 99);
+        assert_eq!(Solution::single_number1(vec![0, 0, 0, 1, 1, 1, 5]), 5);
     }
 }

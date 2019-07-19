ff6f559ec3052689b08e0934d1391d5b7442700d
HAOYUatHZ <haoyu@protonmail.com>
2019-07-19 16:39:37 +0800

    init

--- kamyu104/src/bitwise_and_of_numbers_range.rs
+++ kamyu104/src/bitwise_and_of_numbers_range.rs
@@ -1,1 +1,18 @@
+pub struct Solution {}
 
+impl Solution {
+    pub fn range_bitwise_and(_a: i32, _b: i32) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_range_bitwise_and() {
+        assert_eq!(Solution::range_bitwise_and(1, 2), 3);
+        assert_ne!(Solution::range_bitwise_and(1, 2), 4);
+    }
+}

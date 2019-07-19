2252c2887efe87a9c5ae6382feff67a99236737f
HAOYUatHZ <haoyu@protonmail.com>
2019-07-19 16:44:41 +0800

    fix range_bitwise_and

--- kamyu104/src/bitwise_and_of_numbers_range.rs
+++ kamyu104/src/bitwise_and_of_numbers_range.rs
@@ -1,8 +1,14 @@
+// Time:  O(1)
+// Space: O(1)
+
 pub struct Solution {}
 
 impl Solution {
-    pub fn range_bitwise_and(_a: i32, _b: i32) -> i32 {
-        0
+    pub fn range_bitwise_and(m: i32, mut n: i32) -> i32 {
+        while m < n {  // Remove the last bit 1 until n <= m.
+            n &= n - 1;
+        }
+        n
     }
 }
 
@@ -12,7 +18,7 @@ mod tests {
 
     #[test]
     fn test_range_bitwise_and() {
-        assert_eq!(Solution::range_bitwise_and(1, 2), 3);
-        assert_ne!(Solution::range_bitwise_and(1, 2), 4);
+        assert_eq!(Solution::range_bitwise_and(5, 7), 4);
+        assert_eq!(Solution::range_bitwise_and(0, 1), 0);
     }
 }

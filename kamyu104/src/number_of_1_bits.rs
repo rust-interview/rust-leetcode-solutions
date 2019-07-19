86ace486d738ce701226c8156a2b9d82f4ca6009
HAOYUatHZ <haoyu@protonmail.com>
2019-07-19 13:49:59 +0800

    init hamming_weight

---
+++ kamyu104/src/number_of_1_bits.rs
@@ -0,0 +1,18 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn hamming_weight(n: i32) -> i32 {
+        n
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(Solution::hamming_weight(3), 3);
+        assert_eq!(Solution::hamming_weight(4), 4);
+    }
+}

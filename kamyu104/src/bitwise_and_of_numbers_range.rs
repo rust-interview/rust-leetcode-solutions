d1e3606782a9f5352abe8541c9cf0b8ca7bba488
HAOYUatHZ <haoyu@protonmail.com>
2019-07-19 16:58:20 +0800

    add sol2

--- kamyu104/src/bitwise_and_of_numbers_range.rs
+++ kamyu104/src/bitwise_and_of_numbers_range.rs
@@ -1,10 +1,20 @@
 // Time:  O(1)
 // Space: O(1)
 
-pub struct Solution {}
+pub struct Solution1 {}
+pub struct Solution2 {}
 
-impl Solution {
-    pub fn range_bitwise_and(m: i32, mut n: i32) -> i32 {
+impl Solution1 {
+    pub fn range_bitwise_and(m: u32, mut n: u32) -> u32 {
+        while m < n {  // Remove the last bit 1 until n <= m.
+            n &= n - 1;
+        }
+        n
+    }
+}
+
+impl Solution2 {
+    pub fn range_bitwise_and(m: u32, mut n: u32) -> u32 {
         while m < n {  // Remove the last bit 1 until n <= m.
             n &= n - 1;
         }
@@ -18,7 +28,10 @@ mod tests {
 
     #[test]
     fn test_range_bitwise_and() {
-        assert_eq!(Solution::range_bitwise_and(5, 7), 4);
-        assert_eq!(Solution::range_bitwise_and(0, 1), 0);
+        assert_eq!(Solution1::range_bitwise_and(5, 7), 4);
+        assert_eq!(Solution1::range_bitwise_and(0, 1), 0);
+
+        assert_eq!(Solution2::range_bitwise_and(5, 7), 4);
+        assert_eq!(Solution2::range_bitwise_and(0, 1), 0);
     }
 }

ce474e282adf62f79d21ad79fd2ebdb4d905b79b
HAOYUatHZ <haoyu@protonmail.com>
2019-07-20 15:46:34 +0800

    foreach

--- kamyu104/src/single_number_iii.rs
+++ kamyu104/src/single_number_iii.rs
@@ -7,9 +7,8 @@ impl Solution {
         let x_xor_y: i32 = nums.iter().fold(0, |acc, &num| acc ^ num);
         let bit: i32 = x_xor_y & -x_xor_y;
         let mut result = vec![0; 2];
-        for i in nums {
-            result[((i & bit) != 0) as usize] ^= i
-        }
+        nums.iter()
+            .for_each(|i| result[((i & bit) != 0) as usize] ^= i);
         result
     }
 }

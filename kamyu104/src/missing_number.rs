972cd52831025bc531bb600c38e31eb61dd5bbc8
HAOYUatHZ <haoyu@protonmail.com>
2019-07-20 17:24:18 +0800

    add sol4

--- kamyu104/src/missing_number.rs
+++ kamyu104/src/missing_number.rs
@@ -31,12 +31,12 @@ impl Solution1 {
 
 // Time:  O(n)
 // Space: O(1)
-// pub struct Solution4 {}
-// impl Solution4 {
-//     pub fn missing_number(nums: Vec<i32>) -> i32 {
-//         0
-//     }
-// }
+pub struct Solution4 {}
+impl Solution4 {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        ((0..nums.len() + 1).fold(0, |acc, i| acc + (i as i32))) - nums.iter().sum::<i32>()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -62,10 +62,10 @@ mod tests {
         //     8
         // );
 
-        // assert_eq!(Solution4::missing_number(vec![3, 0, 1]), 2);
-        // assert_eq!(
-        //     Solution2::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]),
-        //     8
-        // );
+        assert_eq!(Solution4::missing_number(vec![3, 0, 1]), 2);
+        assert_eq!(
+            Solution4::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]),
+            8
+        );
     }
 }

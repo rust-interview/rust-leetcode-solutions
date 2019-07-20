c96979996dab1676efaf368653dd63e283fed375
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2019-07-20 18:20:38 +0800

    update readme (#15)

--- kamyu104/src/main.rs
+++ kamyu104/src/main.rs
@@ -1,4 +1,5 @@
 mod bitwise_and_of_numbers_range;
+mod maximum_product_of_word_lengths;
 mod missing_number;
 mod number_of_1_bits;
 mod power_of_two;
---
+++ kamyu104/src/maximum_product_of_word_lengths.rs
@@ -0,0 +1,18 @@
+pub struct Solution {}
+impl Solution {
+    pub fn max_product(words: Vec<String>) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_product() {
+        // assert_max_product!(Solution::max_product(vec!["abcw","baz","foo","bar","xtfn","abcdef"]), 16);
+        // assert_max_product!(Solution::max_product(vec!["a","ab","abc","d","cd","bcd","abcd"]), 4);
+        // assert_max_product!(Solution::max_product(vec!["a","aa","aaa","aaaa"]), 0);
+    }
+}

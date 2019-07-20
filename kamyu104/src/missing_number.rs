810d3bdc13536bb9c2749daddc888211bcdc5458
HAOYUatHZ <haoyu@protonmail.com>
2019-07-20 16:34:38 +0800

    add

--- kamyu104/src/missing_number.rs
+++ kamyu104/src/missing_number.rs
@@ -1,1 +1,17 @@
+pub struct Solution {}
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        0
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_missing_number() {
+        assert_eq!(Solution::missing_number(vec![3, 0, 1]), 2);
+        assert_eq!(Solution::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]), 8);
+    }
+}

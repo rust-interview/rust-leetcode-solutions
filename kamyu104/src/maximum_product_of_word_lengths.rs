b1a1712b755c5a89ea424be2d922a35938210360
HAOYUatHZ <haoyu@protonmail.com>
2019-07-20 18:07:32 +0800

    update

---
+++ kamyu104/src/maximum_product_of_word_lengths.rs
@@ -0,0 +1,16 @@
+pub struct Solution {}
+impl Solution {
+    pub fn max_product(words: Vec<String>) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_max_product() {
+    }
+}

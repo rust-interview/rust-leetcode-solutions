bcdaf0dc53ea696ce005f3ee28e4e1e760e02736
HAOYUatHZ <haoyu@protonmail.com>
2019-07-23 09:31:07 +0800

    minor

--- kamyu104/src/maximum_product_of_word_lengths.rs
+++ kamyu104/src/maximum_product_of_word_lengths.rs
@@ -15,6 +15,8 @@ impl Solution1 {
 pub struct Solution2 {}
 impl Solution2 {
     pub fn max_product(words: Vec<String>) -> i32 {
+        let mut ws = words.clone();
+        ws.sort_by(|a, b| a.len().cmp(&b.len()));
         0
     }
 }
@@ -27,6 +29,10 @@ mod tests {
     fn test_max_product() {
         // assert_eq!(Solution1::max_product(vec!["abcw","baz","foo","bar","xtfn","abcdef"].iter().map(|&x| String::from(x)).collect()), 16);
         // assert_eq!(Solution1::max_product(vec!["a","ab","abc","d","cd","bcd","abcd"].iter().map(|&x| String::from(x)).collect()), 4);
-        // assert_eq!(Solution::max_product(vec!["a","aa","aaa","aaaa"].iter().map(|&x| String::from(x)).collect()), 0);
+        // assert_eq!(Solution1::max_product(vec!["a","aa","aaa","aaaa"].iter().map(|&x| String::from(x)).collect()), 0);
+
+        // assert_eq!(Solution2::max_product(vec!["abcw","baz","foo","bar","xtfn","abcdef"].iter().map(|&x| String::from(x)).collect()), 16);
+        // assert_eq!(Solution2::max_product(vec!["a","ab","abc","d","cd","bcd","abcd"].iter().map(|&x| String::from(x)).collect()), 4);
+        // assert_eq!(Solution2::max_product(vec!["a","aa","aaa","aaaa"].iter().map(|&x| String::from(x)).collect()), 0);
     }
 }

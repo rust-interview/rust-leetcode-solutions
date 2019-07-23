81ea210f2b29bca6f6756c9ca03c2dcc803e1f75
HAOYUatHZ <haoyu@protonmail.com>
2019-07-23 18:39:21 +0800

    ???

--- kamyu104/src/maximum_product_of_word_lengths.rs
+++ kamyu104/src/maximum_product_of_word_lengths.rs
@@ -17,19 +17,26 @@ impl Solution2 {
         let mut sorted_words = words.clone();
         sorted_words.sort_by(|a, b| a.len().cmp(&b.len()));
         let mut bits: Vec<i32> = vec![0; words.len()];
-        for i in 0..sorted_words.len(){
-            for c in sorted_words[i].chars(){
-                bits[i] |= (1 << (c as u32 - 'a' as u32))
+        for i in 0..sorted_words.len() {
+            for c in sorted_words[i].chars() {
+                bits[i] |= 1 << (c as u32 - 'a' as u32);
             }
         }
-        let max_product :i32 = 0;
-        // for (int i = 0; i + 1 < words.size() && pow(words[i].length(), 2) > max_product; ++i) {
-        //     for (int j = i + 1; j < words.size() && words[i].length() * words[j].length() > max_product; ++j) {
-        //         if (!(bits[i] & bits[j])) {
-        //             max_product = words[i].length() * words[j].length();
-        //         }
-        //     }
-        // }
+        let mut max_product: i32 = 0;
+        let mut i: usize = 0;
+        while i + 1 < sorted_words.len() && sorted_words[i].len().pow(2) as i32 > max_product {
+            let mut j: usize = i + 1;
+            while j < sorted_words.len()
+                && (sorted_words[i].len() * sorted_words[j].len()) as i32 > max_product
+            {
+                if bits[i] & bits[j] == 0 {
+                    max_product = (sorted_words[i].len() * sorted_words[j].len()) as i32;
+                }
+                j += 1;
+            }
+
+            i += 1;
+        }
         max_product
     }
 }
@@ -40,9 +47,33 @@ mod tests {
 
     #[test]
     fn test_max_product() {
-        // assert_eq!(Solution1::max_product(vec!["abcw","baz","foo","bar","xtfn","abcdef"].iter().map(|&x| String::from(x)).collect()), 16);
-        // assert_eq!(Solution1::max_product(vec!["a","ab","abc","d","cd","bcd","abcd"].iter().map(|&x| String::from(x)).collect()), 4);
-        // assert_eq!(Solution1::max_product(vec!["a","aa","aaa","aaaa"].iter().map(|&x| String::from(x)).collect()), 0);
+        assert_eq!(
+            Solution2::max_product(
+                vec!["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+                    .iter()
+                    .map(|&x| String::from(x))
+                    .collect()
+            ),
+            16
+        );
+        assert_eq!(
+            Solution2::max_product(
+                vec!["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
+                    .iter()
+                    .map(|&x| String::from(x))
+                    .collect()
+            ),
+            4
+        );
+        assert_eq!(
+            Solution2::max_product(
+                vec!["a", "aa", "aaa", "aaaa"]
+                    .iter()
+                    .map(|&x| String::from(x))
+                    .collect()
+            ),
+            0
+        );
 
         // assert_eq!(Solution2::max_product(vec!["abcw","baz","foo","bar","xtfn","abcdef"].iter().map(|&x| String::from(x)).collect()), 16);
         // assert_eq!(Solution2::max_product(vec!["a","ab","abc","d","cd","bcd","abcd"].iter().map(|&x| String::from(x)).collect()), 4);

4febd4d61c75de06254a9323c21b15bde5978aae
HAOYUatHZ <haoyu@protonmail.com>
2019-07-24 10:16:27 +0800

    update is_power_of_four

--- kamyu104/src/power_of_four.rs
+++ kamyu104/src/power_of_four.rs
@@ -1,1 +1,18 @@
+pub struct Solution {}
+impl Solution {
+    pub fn is_power_of_four(num: i32) -> bool {
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_power_of_four() {
+        assert_eq!(Solution::is_power_of_four(16), true);
+        assert_eq!(Solution::is_power_of_four(5), false);
+    }
+}
 

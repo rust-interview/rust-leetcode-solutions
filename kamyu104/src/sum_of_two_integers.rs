9fe0f8e61c2a7164b1d7dd8d18f14ea47e683d2e
HAOYUatHZ <haoyu@protonmail.com>
2019-07-24 13:45:33 +0800

    add get_sum sol1

--- kamyu104/src/sum_of_two_integers.rs
+++ kamyu104/src/sum_of_two_integers.rs
@@ -1,7 +1,16 @@
+// Time:  O(1)
+// Space: O(1)
 pub struct Solution {}
 impl Solution {
     pub fn get_sum(a: i32, b: i32) -> i32 {
-        0
+        let mut ma = a.clone();
+        let mut mb = b.clone();
+        while mb != 0 {
+            let carry = ma & mb;
+            ma ^= mb;
+            mb = carry << 1;
+        }
+        ma
     }
 }
 

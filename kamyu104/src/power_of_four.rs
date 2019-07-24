1b03a30e248f18ed39b02c985ca9beb8233b48e5
HAOYUatHZ <haoyu@protonmail.com>
2019-07-24 11:17:11 +0800

    add sol2

--- kamyu104/src/power_of_four.rs
+++ kamyu104/src/power_of_four.rs
@@ -3,8 +3,20 @@
 pub struct Solution1 {}
 impl Solution1 {
     pub fn is_power_of_four(num: i32) -> bool {
-        num > 0 && (num & (num - 1)) == 0 &&
-               ((num & 0b01010101010101010101010101010101) == num)
+        num > 0 && (num & (num - 1)) == 0 && ((num & 0b01010101010101010101010101010101) == num)
+    }
+}
+
+// Time:  O(1)
+// Space: O(1)
+pub struct Solution2 {}
+impl Solution2 {
+    pub fn is_power_of_four(num: i32) -> bool {
+        let mut n = num;
+        while (n != 0) && (n & 0b11) == 0 {
+            n >>= 2;
+        }
+        (n == 1)
     }
 }
 
@@ -16,6 +28,8 @@ mod tests {
     fn test_is_power_of_four() {
         assert_eq!(Solution1::is_power_of_four(16), true);
         assert_eq!(Solution1::is_power_of_four(5), false);
+
+        assert_eq!(Solution2::is_power_of_four(16), true);
+        assert_eq!(Solution2::is_power_of_four(5), false);
     }
 }
-

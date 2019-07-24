61d04ac652d36f567b585b83e0b30b0c4b6443e4
HAOYUatHZ <haoyu@protonmail.com>
2019-07-24 18:36:18 +0800

    wip

--- kamyu104/src/sum_of_two_integers.rs
+++ kamyu104/src/sum_of_two_integers.rs
@@ -1,7 +1,8 @@
 // Time:  O(1)
 // Space: O(1)
-pub struct Solution {}
-impl Solution {
+
+pub struct Solution1 {}
+impl Solution1 {
     pub fn get_sum(a: i32, b: i32) -> i32 {
         let mut ma = a.clone();
         let mut mb = b.clone();
@@ -14,6 +15,45 @@ impl Solution {
     }
 }
 
+pub struct Solution2 {}
+impl Solution2 {
+    pub fn get_sum(a: i32, b: i32) -> i32 {
+        let bit_length = 32;
+        let neg_bit = (1 << bit_length) >> 1;
+        let mask = !(!0 << bit_length);
+
+        let mut ma = a & mask;
+        if a & neg_bit != 0 {
+            ma = a | !mask;
+        }
+
+        let mut mb = b & mask;
+        if b & neg_bit != 0 {
+            mb = b | !mask;
+        }
+
+        while mb != 0 {
+            let carry = ma & mb;
+            ma ^= mb;
+            if ma & neg_bit != 0 {
+                ma = ma | !mask;
+            } else {
+                ma = ma & mask;
+            }
+            mb = carry << 1;
+            if mb & neg_bit != 0 {
+                mb = mb | !mask;
+            } else {
+                mb = mb & mask;
+            }
+        }
+        a
+    }
+}
+
+pub struct Solution3 {}
+impl Solution3 {}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -21,7 +61,10 @@ mod tests {
 
     #[test]
     fn test_get_sum() {
-        assert_eq!(Solution::get_sum(1, 2), 3);
-        assert_eq!(Solution::get_sum(-2, 3), 1);
+        assert_eq!(Solution1::get_sum(1, 2), 3);
+        assert_eq!(Solution1::get_sum(-2, 3), 1);
+
+        assert_eq!(Solution2::get_sum(1, 2), 3);
+        assert_eq!(Solution2::get_sum(-2, 3), 1);
     }
 }

8322ca4287d50e29b8a619924e6698ff5dbb4f3e
HAOYUatHZ <haoyu@protonmail.com>
2019-07-24 08:01:49 +0800

    clean

--- kamyu104/src/maximum_product_of_word_lengths.rs
+++ kamyu104/src/maximum_product_of_word_lengths.rs
@@ -1,6 +1,7 @@
 // Time:  O(n) ~ O(n^2)
 // Space: O(n)
 // Counting Sort + Pruning + Bit Manipulation
+// https://github.com/kamyu104/LeetCode-Solutions/blob/master/C++/maximum-product-of-word-lengths.cpp#L1-L40
 pub struct Solution1 {}
 impl Solution1 {
     pub fn max_product(words: Vec<String>) -> i32 {
@@ -37,6 +38,28 @@ impl Solution2 {
             i += 1;
         }
         max_product
+    }
+}
+
+
+// # Time:  O(n) ~ O(n^2)
+// # Space: O(n)
+// https://github.com/kamyu104/LeetCode-Solutions/blob/master/Python/maximum-product-of-word-lengths.py#L1-L36
+pub struct Solution3 {}
+impl Solution3 {
+    pub fn max_product(words: Vec<String>) -> i32 {
+        0
+    }
+}
+
+// # Time:  O(nlogn) ~ O(n^2)
+// # Space: O(n)
+// # Sorting + Pruning + Bit Manipulation
+// https://github.com/kamyu104/LeetCode-Solutions/blob/master/Python/maximum-product-of-word-lengths.py#L38-L62
+pub struct Solution4 {}
+impl Solution4 {
+    pub fn max_product(words: Vec<String>) -> i32 {
+        0
     }
 }
 

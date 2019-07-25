0e87d6ee4483353b5f50db18ab7f86fdb7dfba41
HAOYUatHZ <haoyu@protonmail.com>
2019-07-25 08:18:14 +0800

    draft

---
+++ kamyu104/src/find_the_difference.rs
@@ -0,0 +1,17 @@
+pub struct Solution {}
+impl Solution {
+    pub fn find_the_difference(s: String, t: String) -> char {
+        'e'
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_find_the_difference() {
+        assert_eq!(Solution::find_the_difference("abcd".to_string(), "abcde".to_string()), 'e');
+    }
+}

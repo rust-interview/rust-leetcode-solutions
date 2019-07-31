61a62cf7a124a433ca2a8a122fb4cb4e99d9d8a0
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-07-31 18:56:17 +0800

    init test_read_binary_watch

---
+++ kamyu104/src/binary_watch.rs
@@ -0,0 +1,19 @@
+pub struct Solution {}
+impl Solution {
+    // pub fn read_binary_watch(num: i32) -> Vec<String> {
+    //     vec![]
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_binary_watch() {
+        // assert_eq!(
+        //     Solution::read_binary_watch(1),
+        //     vec!["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+        // );
+    }
+}

bd522ea88a1640f77d26d6ef64fffbceb2862cd2
HAOYUatHZ <haoyu@protonmail.com>
2019-07-31 10:56:00 +0800

    add utf_8_validation

--- kamyu104/src/utf_8_validation.rs
+++ kamyu104/src/utf_8_validation.rs
@@ -1,7 +1,31 @@
+// Time:  O(n)
+// Space: O(1)
 pub struct Solution {}
 impl Solution {
     pub fn valid_utf8(data: Vec<i32>) -> bool {
-        true
+        let mut count: i32 = 0;
+        for c in data {
+            match count {
+                0 => {
+                    if (c >> 5) == 0b110 {
+                        count = 1;
+                    } else if (c >> 4) == 0b1110 {
+                        count = 2;
+                    } else if (c >> 3) == 0b11110 {
+                        count = 3;
+                    } else if c >> 7!=0 {
+                        return false;
+                    }
+                },
+                _ => {
+                    if (c >> 6) != 0b10 {
+                        return false;
+                    }
+                    count-=1;
+                },
+            }
+        }
+        count == 0
     }
 }
 

0f17beba9a5c799a10853b3243eb2f36015d2097
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-07-31 22:00:55 +0800

    add testcases

--- kamyu104/src/binary_watch.rs
+++ kamyu104/src/binary_watch.rs
@@ -1,8 +1,8 @@
 pub struct Solution {}
 impl Solution {
-    // pub fn read_binary_watch(num: i32) -> Vec<String> {
-    //     vec![]
-    // }
+    pub fn read_binary_watch(num: i32) -> Vec<String> {
+        vec![]
+    }
 }
 
 #[cfg(test)]
@@ -11,9 +11,19 @@ mod tests {
 
     #[test]
     fn test_read_binary_watch() {
-        // assert_eq!(
-        //     Solution::read_binary_watch(1),
-        //     vec!["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
-        // );
+        assert_eq!(
+            Solution::read_binary_watch(1),
+            vec!["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+                .iter()
+                .map(|&x| String::from(x))
+                .collect::<Vec<String>>()
+        );
+        assert_eq!(
+            Solution::read_binary_watch(1),
+            vec!["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+                .iter()
+                .map(|&x| x.into())
+                .collect::<Vec<String>>()
+        );
     }
 }

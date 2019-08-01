39587a17b325eb5ea7aa421555fd27ca9e6f83dd
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-08-01 20:27:13 +0800

    add

--- kamyu104/src/maximum_xor_of_two_numbers_in_an_array.rs
+++ kamyu104/src/maximum_xor_of_two_numbers_in_an_array.rs
@@ -1,1 +1,16 @@
+pub struct Solution {}
+impl Solution {
+    pub fn find_maximum_xor(nums: Vec<i32>) -> i32 {
+        0
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_maximum_xor() {
+        assert_eq!(Solution::find_maximum_xor(vec![3, 10, 5, 25, 2, 8]), 28);
+    }
+}

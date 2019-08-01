bdb75f5f8b31e0b2741a79063484e99724da52f8
HAOYUatHZ <haoyu@protonmail.com>
2019-08-01 14:01:14 +0800

    minor

--- kamyu104/src/binary_watch.rs
+++ kamyu104/src/binary_watch.rs
@@ -1,7 +1,26 @@
 pub struct Solution {}
 impl Solution {
     pub fn read_binary_watch(num: i32) -> Vec<String> {
-        vec![]
+        // vec![]
+        let result: Vec<String> = Vec::new();
+        /*for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (bit_count(h) + bit_count(m) == num) {
+                    const auto hour = to_string(h);
+                    const auto minute = m < 10 ? "0" + to_string(m) : to_string(m);
+                    result.emplace_back(hour + ":" + minute);
+                }
+            }
+        }*/
+        result
+    }
+
+    fn bit_count(bits: i32) -> i32 {
+        let mut count: i32 = 0;
+        // for (; bits; bits &= bits - 1) {
+        //     ++count;
+        // }
+        count
     }
 }
 

357b3c6324880f50adb46c0f7b6ddd8ed83b7420
HAOYUatHZ <haoyu@protonmail.com>
2019-08-01 16:06:27 +0800

    clean up

--- kamyu104/src/binary_watch.rs
+++ kamyu104/src/binary_watch.rs
@@ -1,25 +1,24 @@
 pub struct Solution {}
 impl Solution {
     pub fn read_binary_watch(num: i32) -> Vec<String> {
-        // vec![]
-        let result: Vec<String> = Vec::new();
-        /*for (int h = 0; h < 12; ++h) {
-            for (int m = 0; m < 60; ++m) {
-                if (bit_count(h) + bit_count(m) == num) {
-                    const auto hour = to_string(h);
-                    const auto minute = m < 10 ? "0" + to_string(m) : to_string(m);
-                    result.emplace_back(hour + ":" + minute);
+        let mut result: Vec<String> = Vec::new();
+        (0..12).for_each(|h| {
+            (0..60).for_each(|m| {
+                if (Solution::bit_count(h) + Solution::bit_count(m)) == num {
+                    let elem = format!("{}:{:>02}", h, m);
+                    result.push(elem);
                 }
-            }
-        }*/
+            });
+        });
         result
     }
 
-    fn bit_count(bits: i32) -> i32 {
+    fn bit_count(mut bits: i32) -> i32 {
         let mut count: i32 = 0;
-        // for (; bits; bits &= bits - 1) {
-        //     ++count;
-        // }
+        while bits != 0 {
+            count += 1;
+            bits &= bits - 1;
+        }
         count
     }
 }
@@ -32,14 +31,15 @@ mod tests {
     fn test_read_binary_watch() {
         assert_eq!(
             Solution::read_binary_watch(1),
-            vec!["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+            // vec!["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+            vec!["0:01", "0:02", "0:04", "0:08", "0:16", "0:32", "1:00", "2:00", "4:00", "8:00"]
                 .iter()
                 .map(|&x| String::from(x))
                 .collect::<Vec<String>>()
         );
         assert_eq!(
             Solution::read_binary_watch(1),
-            vec!["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+            vec!["0:01", "0:02", "0:04", "0:08", "0:16", "0:32", "1:00", "2:00", "4:00", "8:00"]
                 .iter()
                 .map(|&x| x.into())
                 .collect::<Vec<String>>()

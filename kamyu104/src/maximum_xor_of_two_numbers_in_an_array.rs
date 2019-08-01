c1fc403606d05f196982aaa249c15f0824bebfb7
ChrisLinn <nanyishanshaoxiu@gmail.com>
2019-08-02 06:41:46 +0800

    fix

--- kamyu104/src/maximum_xor_of_two_numbers_in_an_array.rs
+++ kamyu104/src/maximum_xor_of_two_numbers_in_an_array.rs
@@ -1,7 +1,26 @@
+use std::collections::HashSet;
+  
+// Time:  O(n)
+// Space: O(n)
+
 pub struct Solution {}
 impl Solution {
     pub fn find_maximum_xor(nums: Vec<i32>) -> i32 {
-        0
+        let mut result: i32 = 0;
+        for i in (0..32).rev() {
+            result <<= 1;
+            let mut prefixes: HashSet<i32> = HashSet::new();
+            for n in nums.clone() {
+                prefixes.insert(n >> i);
+            }
+            for p in prefixes.clone() {
+                if prefixes.contains(&((result | 1) ^ p)) {
+                    result += 1;
+                    break;
+                }
+            }
+        }
+        result
     }
 }
 

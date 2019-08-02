8a134b03a8d1bb18d3e73f7dffaa756634e2bb6b
HAOYUatHZ <haoyu@protonmail.com>
2019-08-02 15:33:20 +0800

    add

--- kamyu104/src/hamming_distance.rs
+++ kamyu104/src/hamming_distance.rs
@@ -1,1 +1,19 @@
+// Time:  O(1)
+// Space: O(1)
+pub struct Solution {}
+impl Solution {
+    pub fn hamming_distance(x: i32, y: i32) -> i32 {
+        2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hamming_distance() {
+        assert_eq!(Solution::hamming_distance(1, 4), 2);
+    }
+}
 
